9f7ba6e32f187031f40a8e2e4bff783f326568c9
Nguyen Van Sang <Nguyenvansang30102001@gmail.com>
2021-08-06 15:51:07 +0700

    Push json

---
+++ generator-data.js
@@ -0,0 +1,65 @@
+const { fake } = require("faker");
+const faker = require("faker");
+const fs = require("fs");
+faker.locale = "vi";
+
+console.log(faker.commerce.department());
+console.log(faker.commerce.productName());
+console.log(faker.commerce.productDescription());
+console.log(faker.commerce.productAdjective());
+
+console.log(faker.image.imageUrl());
+const Random = (n) => {
+  if (n <= 0) return [];
+
+  const categoryList = [];
+
+  Array.from(new Array(n)).forEach((item) => {
+    const category = {
+      id: faker.random.uuid(),
+      name: faker.commerce.department(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    categoryList.push(category);
+  });
+  return categoryList;
+};
+const randomProduct = (List, number) => {
+  const ProductList = [];
+  if (number <= 0) return [];
+  for (const item of List) {
+    Array.from(new Array(number)).forEach(() => {
+      //   console.log(item);
+      const product = {
+        categoryId: item.id,
+        id: faker.random.uuid(),
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: Number.parseFloat(faker.commerce.price()),
+        desciptions: faker.commerce.productDescription(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        thumbnailUrl: faker.image.imageUrl(400, 400),
+      };
+      ProductList.push(product);
+    });
+  }
+  return ProductList;
+};
+//IIFE
+(() => {
+  const categoryList = Random(4);
+  const productList = randomProduct(categoryList, 5);
+  const db = {
+    categories: categoryList,
+    products: productList,
+    profile: {
+      name: "Po",
+    },
+  };
+  //write db object to db.json
+  fs.writeFile("./db.json", JSON.stringify(db), () => {
+    console.log("Write data succecful");
+  });
+})();
---
+++ main.js
@@ -0,0 +1,52 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const queryString = require('query-string');
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server routers
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === 'PATCH') {
+    req.body.updatedAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+router.render = (req, res) => {
+  // check GET with pagination
+  const headers = res.getHeaders();
+  const Total_count = headers['x-total-count'];
+  if (req.method === 'GET' && Total_count) {
+    // console.log(req);
+    console.log(req._parsedOriginalUrl.query);
+    const queryParams = queryString.parse(req._parsedOriginalUrl.query);
+    console.log(queryParams);
+    const result = {
+      data: res.locals.data,
+      paginations: {
+        _page: queryParams._page || 1,
+        _limit: queryParams._limit || 10,
+        _totalRows: Number.parseInt(Total_count),
+      },
+    };
+    return res.jsonp(result);
+  }
+  res.jsonp(res.locals.data);
+};
+// Use default router
+server.use('/api', router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
